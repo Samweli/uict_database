94830c8b666fa4e0628be5cbf9ed3ceb2fd28496
Samweli <smwltwesa6@gmail.com>
2014-04-06 21:55:31 +0300

    fixed footer

--- public/templates/footer.php
+++ public/templates/footer.php
@@ -3,23 +3,23 @@
                         <div class="u_social">
                             <ul class=" nav u_footer_nav">
                                 <li>
-				<?php echo (CurrentPage::$currentPage == "home")?'<a href="www.facebook.com/uict" > <img src="./public/img/fb-icon.jpg" alt="facebook logo" width="20" height="20" /> Facebook </a>':
+				<?php echo (CurrentPage::$currentPage == "home")?'<a href="www.facebook.com/uict" > <img src="./public/img/fb-icon.jpg" alt="facebook logo" width="20" height="20" /> Facebook </a>':(
 			                   (CurrentPage::$currentPage == "userhome")?'<a href="www.facebook.com/uict" > <img src="../../public/img/fb-icon.jpg" alt="facebook logo" width="20" height="20" /> Facebook </a>':
-					   '<a href="www.facebook.com/uict" > <img src="../public/img/fb-icon.jpg" alt="facebook logo" width="20" height="20" /> Facebook </a>';
+					   '<a href="www.facebook.com/uict" > <img src="../public/img/fb-icon.jpg" alt="facebook logo" width="20" height="20" /> Facebook </a>');
 				?>
 				</li>
                                 <li>
 				<?php
-				  echo (CurrentPage::$currentPage == "home")?'<a href="www.twitter.com/uict" ><img src="./public/img/twitter-icon.jpg" alt="twitter logo" width="20" height="20" /> Twitter </a>':
+				  echo (CurrentPage::$currentPage == "home")?'<a href="www.twitter.com/uict" ><img src="./public/img/twitter-icon.jpg" alt="twitter logo" width="20" height="20" /> Twitter </a>':(
 			               (CurrentPage::$currentPage == "userhome")?'<a href="www.twitter.com/uict" ><img src="../../public/img/twitter-icon.jpg" alt="twitter logo" width="20" height="20" /> Twitter </a>':
-					'<a href="www.twitter.com/uict" ><img src="../public/img/twitter-icon.jpg" alt="twitter logo" width="20" height="20" /> Twitter </a>';
+					'<a href="www.twitter.com/uict" ><img src="../public/img/twitter-icon.jpg" alt="twitter logo" width="20" height="20" /> Twitter </a>');
 			        ?>
 				</li>
                                 <li>
 			        <?php
-				   echo (CurrentPage::$currentPage == "home")?'<a href="www.google+.com/uict" ><img src="./public/img/google.jpg" alt="google+ logo"  width="20" height="20"/> Google+ </a>':
+				   echo (CurrentPage::$currentPage == "home")?'<a href="www.google+.com/uict" ><img src="./public/img/google.jpg" alt="google+ logo"  width="20" height="20"/> Google+ </a>':(
 			                (CurrentPage::$currentPage == "userhome")?'<a href="www.google+.com/uict" ><img src="../../public/img/google.jpg" alt="google+ logo"  width="20" height="20"/> Google+ </a>':
-					'<a href="www.google+.com/uict" ><img src="../public/img/google.jpg" alt="google+ logo"  width="20" height="20"/> Google+ </a>';
+					'<a href="www.google+.com/uict" ><img src="../public/img/google.jpg" alt="google+ logo"  width="20" height="20"/> Google+ </a>');
 				 ?>
 				</li>
                             </ul>
